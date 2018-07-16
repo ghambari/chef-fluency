e2b3bf77a7378e8f11a1a60673b9b6ccffef7e08
root <root@aghambari1.mylabserver.com>
2018-07-16 18:43:02 +0000

    adding to repo

---
+++ learn.rb
@@ -0,0 +1,17 @@
+
+package 'apache' do
+	package_name 'httpd'
+end
+
+service 'httpd' do
+	action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+	content 'hello world!'
+	mode '0755'
+	owner 'root'
+	group 'apache'
+end
+
+
---
+++ workstation/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook:: workstation
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'tree' do
+end
